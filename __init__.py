70476f2046c4f6c159cda045f5b7391855b3d0a1
g-mecha <maydayfan11@gmail.com>
2025-01-20 15:56:57 +0100

    Ghe user can now choose an episode to play

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 import os.path
+import json
 
+from ovos_number_parser import pronounce_number, extract_number
 from ovos_workshop.skills.game_skill import ConversationalGameSkill
 
 
@@ -15,13 +17,54 @@ class MyGameSkill(ConversationalGameSkill):
         self.episode_number = 0
         self.number_of_episodes = 0
 
+        # We don't need this at all. I keep this around for fast debuging
+        # self.gui.show_text(f"{self.data}")
+
+
     def on_play_game(self):
         """called by ocp_pipeline when 'play XXX' matches the game"""
         # os.path.join(os.path.dirname(__file__), "res", "images", "game.png")
         self.number_of_episodes = sum(1 for _, _, files in os.walk(f'{self.root_dir}/resources/episodes') for f in files)
-        self.gui.show_text(f"{self.number_of_episodes}")
 
-        # select_episode(number_of_episodes)
+        self.select_episode()
+
+    def select_episode(self):
+        if  (self.number_of_episodes == 1):
+            self.speak("Only 1 episode detected, playing episode 1")
+            self.open_json_file(1)
+        else:
+            # chosen_episode = self.ask_selection(chosen_episode)
+            self.speak(f"I found {self.number_of_episodes} episodes")
+            chosen_episode_input = self.get_response('What episode do you want to play?')
+            # chosen_episode self.extract_number
+            chosen_episode = extract_number(chosen_episode_input, ordinals=True, lang=self.lang)
+
+            self.gui.show_text(f"{chosen_episode_input}, {chosen_episode}")
+
+            if chosen_episode == False:
+                self.speak("Invalid. Please say a number")
+                self.select_episode()
+            else:
+                chosen_episode_int = int(chosen_episode)
+                if chosen_episode_int <= 0 or chosen_episode_int > self.number_of_episodes:
+                    self.speak(f"Invalid input. Please select an episode between 1 and {self.number_of_episodes}")
+                    self.select_episode()
+                else:
+                    self.speak(f"speel aflevering {chosen_episode_int}")
+                    self.open_json_file(chosen_episode_int)
+
+    def open_json_file(self, chosen_episode_int):
+        
+        # Opening JSON file
+        f = open(f'{self.root_dir}/resources/episodes/Episode{chosen_episode_int}_Data.json')
+
+        # returns JSON object as a dictionary
+        self.episode_data = json.load(f)
+
+        self.gui.show_text(f"{self.episode_data['rooms']['start']}")
+
+        # Closing file
+        f.close()
         
 
     def on_stop_game(self):
